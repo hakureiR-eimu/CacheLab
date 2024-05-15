c7556cfaae78d632a9340c1594cb4f05765ee7a4
CodeOfUsamiWSL <zhang0285@foxmail.com>
2024-05-15 21:25:01 +0800

    feat:Update

--- CacheLab1/src/step1.sh
+++ CacheLab1/src/step1.sh
@@ -5,10 +5,10 @@ rm ./src/csim-ref  > /dev/null 2<&1
 rm ./src/test-csim > /dev/null 2<&1
 cd ./src  > /dev/null 2<&1
 gcc -g -Wall -Werror -std=c99 -m64 -o csim csim.c cachelab.c -lm  >/dev/null 2>&1
-#gcc -g -Wall -Werror -std=c99 -m64 -o build/csim csim-ref.c cachelab.c -lm  >/dev/null 2>&1
+#gcc -g -Wall -Werror -std=c99 -m64 -o build/csim src/csim-ref.c src/cachelab.c -lm  >/dev/null 2>&1
 gcc -g -Wall -Werror -std=c99 -m64 -o csim-ref csim-ref.c cachelab.c -lm
 gcc -g -Wall -Werror -std=c99 -m64 -o test-csim test-csim.c >/dev/null 2>&1
-python driver.py > info 2>&1
+python3 driver.py > info 2>&1
 cat info | grep "程序无效" >result 2>&1
 if [ $? -eq 0 ]; then
     cat info
